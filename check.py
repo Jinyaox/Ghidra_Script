f4706cebcebad3628ac638e202426d972558822b
Jinyaox <103966714+Jinyaox@users.noreply.github.com>
2022-06-26 06:56:10 -0700

    Add files via upload

---
+++ check.py
@@ -0,0 +1,12 @@
+#This is for checking purposes only
+#@author Jinyao Xu
+#@category _NEW_
+#@keybinding 
+#@menupath Tools.Misc.check
+#@toolbar 
+
+
+#TODO Add User Code Here
+
+command=askString("check label","Enter a label string: ")
+print(command)
